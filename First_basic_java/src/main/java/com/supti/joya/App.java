0b3d981ab89e2b67e81dd948b3cc379a540b7b9f
SuptiJoya1 <joya.cse.bubt@gmail.com>
2023-12-29 18:49:14 +0600

    java

---
+++ First_basic_java/src/main/java/com/supti/joya/App.java
@@ -0,0 +1,13 @@
+package com.supti.joya;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
